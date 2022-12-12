88c52cd60c9089f14f32fbf8a863ede790664c73
enricoargenti <105004504+enricoargenti@users.noreply.github.com>
2022-12-12 17:47:03 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib as plt
-import numpy as np
-import sklearn as sl
-
-'''importing train dataset and test dataset'''
-train_df = pd.read_csv(r'C:\Users\Utente\Desktop\TitanicKaggle\csv\train.csv')
-test_df=pd.read_csv(r'C:\Users\Utente\Desktop\TitanicKaggle\csv\test.csv')
-train_df.info()
